8bc0012176f4f884f43fcd3046115adc756e31fb
Dmitry <dmitry@pickle.app>
2019-09-21 13:37:15 +0100

    Initial Commit

---
+++ BarCodeGenerator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BarCodeGenerator
+//
+//  Created by Mu-Sonic on 21/09/2019.
+//  Copyright © 2019 Mu-Sonic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
